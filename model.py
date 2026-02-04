3a9e45082c713738884064ae3d7096b69e0d93f8
gunluthra <hargunkluthra1998@gmail.com>
2026-02-04 23:10:30 +0530

    Implement activity registration and risk evaluation
    Added methods to register activity and evaluate risk score based on inactivity.

--- model.py
+++ model.py
@@ -3,15 +3,22 @@ import time
 class SilentThreatModel:
     def __init__(self):
         self.last_activity = time.time()
+        self.risk_score = 0
 
-    def predict(self):
-        idle_time = time.time() - self.last_activity
+    def register_activity(self):
+        self.last_activity = time.time()
+        self.risk_score = max(0, self.risk_score - 1)
 
-        if idle_time < 5:
+    def evaluate(self):
+        gap = time.time() - self.last_activity
+
+        if gap < 5:
             return "GREEN", "Normal activity detected"
 
-        elif idle_time < 15:
-            return "YELLOW", "Suspicious inactivity"
+        elif gap < 12:
+            self.risk_score += 1
+            return "YELLOW", "Suspicious delay in activity"
 
         else:
+            self.risk_score += 2
             return "RED", "Silent threat detected"
